9aa0deb5949221488ada2360b6359cb532b67309
ShalitaCollins <nyarwayashalitacollin@gmail.com>
2023-09-08 12:09:44 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+        int q, r, s;
+	
+	for (q = '0'; q < '9'; q++)
+	{
+	for (r = '1'; r < '9'; r++)
+	 {
+
+        for (s = r + 1; s <= '9'; s++)
+	 {
+	if ((r != q) != s)
+	{
+        putchar(q);
+        putchar(r);
+        putchar(s);
+        if (q == '7' && r == '8')
+	continue;
+        putchar(',');
+        putchar(' ');
+	}
+	}
+	}
+	}
+        putchar('\n');
+        return (0);
+
+}	
