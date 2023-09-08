23cee8d6e705a7d0264d9dd0cd2823e6b6b08469
ShalitaCollins <nyarwayashalitacollin@gmail.com>
2023-09-08 09:54:23 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int i;
+    for (i = 0; i < 10; i++) {
+        putchar('0' + i);
+        if (i < 9) {
+            putchar(','); 
+            putchar(' '); 
+        }
+    }
+
+    putchar('\n'); 
+
+    return 0;
+}
