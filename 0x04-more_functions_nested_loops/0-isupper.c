53034fe9990be902ca9152fa361925308f70adda
ShalitaCollins <nyarwayashalitacollin@gmail.com>
2023-09-14 04:11:09 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,16 @@
 
 /**
  * _isupper - check if a letter is upper
- * @c: the num to be checked
+ * @x: the num to be checked
  * return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int c)
-
-{
-
-	if (c >= 65 && c <= 90)
-	{
-	return (1);
-	}
-	return (0);
-}
+int _isupper(int y)
+        
+{       
+        
+        if (y >= 65 && y <= 98)
+                return (1);
+	else       
+                return (0);
+}       
